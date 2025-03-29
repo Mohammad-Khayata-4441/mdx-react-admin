19771f08e932b610f67176ed9ab23c85886e6cbd
Mohammad-Khayata-4441 <mohammadkhayata.gm@gmail.com>
2025-03-29 16:31:54 +0300

    new feature : home page demos

--- src/modules/dashboard/components/widgets/StatCard.tsx
+++ src/modules/dashboard/components/widgets/StatCard.tsx
@@ -1,5 +1,77 @@
+import { NorthRounded, People, SouthRounded } from "@mui/icons-material";
+import { alpha, Box, Card, Theme, Typography } from "@mui/material";
 import React from "react";
+export type StatCardProps = {
+  title?: string;
+  value?: string;
+  icon?: React.ReactNode;
+  color?: (t: Theme) => string;
+  description?: string;
+  rate?: number;
+};
+export default function StatCard({
+  color = (t) => t.palette.primary.main,
+  value = "1000",
+  description = "Description placeholder",
+  rate = 6.5,
+  title = "Title",
+  icon = <People />,
+}: StatCardProps) {
+  return (
+    <Card sx={{ p: 2 }}>
+      <Box display={"flex"} justifyContent={"space-between"}>
+        <Box
+          sx={{
+            position: "relative",
+            px: 3,
+            "&::before": {
+              content: "''",
+              borderRadius: "2rem",
+              position: "absolute",
+              height: "100%",
+              width: "5px",
+              backgroundColor: (t) => color(t),
+              left: 0,
+            },
+          }}
+        >
+          <Typography fontSize={16} fontWeight={"600"} color="textDisabled">
+            {title}
+          </Typography>
+          <Typography fontSize={28} fontWeight="bold">
+            {Number(value).toFixed(2)}
+          </Typography>
+        </Box>
 
-export default function StatCard() {
-  return <div>StatCard</div>;
+        <Box
+          bgcolor={(t) => alpha(color(t), 0.1)}
+          color={(t) => color(t)}
+          borderRadius={1}
+          p={1}
+          sx={{ width: 40, height: 40 }}
+        >
+          {icon}
+        </Box>
+      </Box>
+      <Box display={"flex"} mt={2}>
+        {rate > 0 ? (
+          <NorthRounded sx={{ fontSize: 18 }} color="success"></NorthRounded>
+        ) : (
+          <SouthRounded sx={{ fontSize: 18 }} color="error"></SouthRounded>
+        )}
+        <Typography
+          variant="body2"
+          color={rate > 0 ? "success" : "error"}
+          mr={1}
+          display={"inline"}
+        >
+          {rate > 0 ? "+" : "-"}
+          {Number(rate).toFixed(2)}%
+        </Typography>
+        <Typography variant="body2" color="textDisabled">
+          {description}
+        </Typography>
+      </Box>
+    </Card>
+  );
 }
--- src/modules/home/page.tsx
+++ src/modules/home/page.tsx
@@ -1,42 +1,43 @@
 import { Card, CardContent, CardHeader, Grid2 } from "@mui/material";
+import StatCard from "../dashboard/components/widgets/StatCard";
+import TickPlacementBars from "../dashboard/components/widgets/charts-demos/Bars";
+import BasicLineChart from "../dashboard/components/widgets/charts-demos/Line";
 
 export default function Home() {
   return (
     <Grid2 container spacing={1}>
-      <Grid2 size={3}>
+      *
+      <Grid2 container size={12} position={"relative"} spacing={1}>
+        {/* <Box sx={{ position: "absolute", top: 0, left: 0, width: "100%" }}>
+          <img src="/images/bg/1.jpg" />
+        </Box> */}
+        <Grid2 size={{ xs: 12, md: 6, lg: 3, xl: 3 }}>
+          <StatCard></StatCard>
+        </Grid2>
+        <Grid2 size={{ xs: 12, md: 6, lg: 3, xl: 3 }}>
+          <StatCard></StatCard>
+        </Grid2>
+        <Grid2 size={{ xs: 12, md: 6, lg: 3, xl: 3 }}>
+          <StatCard></StatCard>
+        </Grid2>
+        <Grid2 size={{ xs: 12, md: 6, lg: 3, xl: 3 }}>
+          <StatCard></StatCard>
+        </Grid2>
+      </Grid2>
+      <Grid2 size={7}>
         <Card>
-          <CardHeader title="Stat 1"></CardHeader>
-          <CardContent>Stat 1 Content</CardContent>
+          <CardHeader title="Monthly Rainfall"></CardHeader>
+          <CardContent>
+            <TickPlacementBars />
+          </CardContent>
         </Card>
       </Grid2>
-      <Grid2 size={3}>
+      <Grid2 size={5}>
         <Card>
-          <CardHeader title="Stat 1"></CardHeader>
-          <CardContent>Stat 1 Content</CardContent>
-        </Card>
-      </Grid2>
-      <Grid2 size={3}>
-        <Card>
-          <CardHeader title="Stat 1"></CardHeader>
-          <CardContent>Stat 1 Content</CardContent>
-        </Card>
-      </Grid2>
-      <Grid2 size={3}>
-        <Card>
-          <CardHeader title="Stat 1"></CardHeader>
-          <CardContent>Stat 1 Content</CardContent>
-        </Card>
-      </Grid2>
-      <Grid2 size={8}>
-        <Card>
-          <CardHeader title="Stat 1"></CardHeader>
-          <CardContent>Stat 1 Content</CardContent>
-        </Card>
-      </Grid2>
-      <Grid2 size={4}>
-        <Card>
-          <CardHeader title="Stat 1"></CardHeader>
-          <CardContent>Stat 1 Content</CardContent>
+          <CardHeader title="Line Chart "></CardHeader>
+          <CardContent>
+            <BasicLineChart />
+          </CardContent>
         </Card>
       </Grid2>
       <Grid2 size={8} container spacing={2}>
